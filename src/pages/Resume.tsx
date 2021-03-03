6c9999a8243f59b3977e8b47d8dba4e32b8b34bb
AmSmo <adam.smolenski@gmail.com>
2021-03-02 20:34:30 -0500

    with resume

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,9 +1,30 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import { RemBody, ZoomCtrl } from "../styles/styles";
+import "react-pdf/dist/umd/Page/AnnotationLayer.css";
 type ResumeProps = {};
 
 const Resume: React.FC<ResumeProps> = () => {
-  return <div>Resume</div>;
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  const [scale, setScale] = useState<number>(1);
+  return (
+    <>
+      <ZoomCtrl>
+        <span onClick={() => setScale((prevScale) => prevScale + 0.1)}>+</span>
+
+        <span onClick={() => setScale((prevScale) => prevScale - 0.1)}>-</span>
+      </ZoomCtrl>
+      <RemBody>
+        <Document
+          className="resume"
+          file={"../Static/CurrentResume.pdf"}
+          externalLinkTarget={"_blank"}
+        >
+          <Page pageNumber={1} scale={scale} />
+        </Document>
+      </RemBody>
+    </>
+  );
 };
 
 export default Resume;
--- src/styles/styles.tsx
+++ src/styles/styles.tsx
@@ -15,3 +15,45 @@ export const NavButton = styled.p`
   text-align: center;
   margin: 0.4em;
 `;
+
+export const RemBody = styled.section`
+  margin: 5px auto;
+  text-align: center;
+  width: 100vw;
+  display: flexbox;
+  justify-content: center;
+  margin-top: 30px;
+  z-index: 3;
+`;
+
+export const ZoomCtrl = styled.div`
+  display: block;
+  font-size: 3rem;
+  margin-top: 0.5rem;
+  width: 100vw;
+  margin-right: auto;
+  text-align: right;
+  height: 2.2rem;
+  span {
+    border-radius: 15px;
+    width: 2rem;
+    padding: 0 1.2rem;
+    margin: 10px;
+    display: inline;
+    background: #28539c;
+    color: white;
+    border: 0.5px solid black;
+    box-shadow: 2px 2px 7px black;
+    z-index: 20;
+    cursor: pointer;
+    :active {
+      box-shadow: 1px 1 px 2px black;
+      transition: 0.2s;
+    }
+    :hover {
+      color: #cfa9a9;
+      background: #214b94;
+      transition-duration: 0.7s;
+    }
+  }
+`;
