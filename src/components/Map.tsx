4c68f020010a1b6ed521ce6957d0459f2abe9b0c
Adam Smolenski <adam.smolenski@gmail.com>
2021-03-14 01:43:22 +0000

    geojson overlay

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,22 @@
 import React from "react"
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
+import {features} from './world.json'
 
 type MapProps = {
 
 }
 
 const Map:React.FC<MapProps> = () => {
+   
+      
+    const onEachFeature= (feature, layer)=> {
+        layer.on({
+        //     mouseover: highlightFeature,
+        //     mouseout: resetHighlight,
+        click: () => console.log(feature.properties.NAME)
+        });
+        
+      }
   return (
       <div id="mapid"> 
    <MapContainer center={[10.505, -10.09]} zoom={1} scrollWheelZoom={false}>
@@ -14,6 +24,8 @@ const Map:React.FC<MapProps> = () => {
     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
+   <GeoJSON data={features as any} onEachFeature={onEachFeature}
+                   />
   <Marker position={[51.505, -0.09]}>
     <Popup>
       A pretty CSS3 popup. <br /> Easily customizable.
--- src/util/customtypes.d.ts
+++ src/util/customtypes.d.ts
@@ -8,6 +8,8 @@ export type SwitchProps = {
   location: Location;
 };
 
+
+
 export type LanguageType = {
   icon: string;
   name: string;
